1f364fca7bb950acabc91db65a9afa40f8ac85f3
viniltc <viniltc@gmail.com>
2019-11-05 13:54:59 +0000

    minor

--- POML060_v1/stageonemain.cpp
+++ POML060_v1/stageonemain.cpp
@@ -50,7 +50,7 @@ StageOneMain::StageOneMain(QWidget *parent) : QMainWindow(parent)
              // statusBar()->showMessage("Config file sent", 3000);
 
           }
-//test git
+//test git git
 
       tetra_grip_api::battery_percentage();
 }
